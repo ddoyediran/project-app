1d31419ea61753cc0b5baa90c90160fbf3f1b3f0
ddoyediran <ddoyediran@gmail.com>
2023-05-17 00:23:16 +0200

    Added pages

---
+++ pages/api/signin.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "@/lib/db";
+import { comparePassword, createJWT } from "@/lib/auth";
+import { serialize } from "cookie";
+
+export default async function signin(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const user = await db.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid login" });
+    }
+
+    const isUser = await comparePassword(req.body.password, user.password);
+
+    if (!isUser) {
+      return res.status(401).json({ error: "Invalid login" });
+    }
+
+    const jwt = await createJWT(user);
+
+    res.setHeader(
+      "Set-Cookie",
+      serialize(process.env.COOKIE_NAME, jwt, {
+        httpOnly: true,
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7,
+      })
+    );
+
+    res.status(201);
+    res.json({});
+  } else {
+    res.status(402);
+    res.json({});
+  }
+}
