062bf2ae2aa067d69c9c5bae538d4807b00c5649
ddoyediran <ddoyediran@gmail.com>
2023-05-16 23:50:44 +0200

    app updated

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import GlassPane from "@/components/GlassPane";
 import { Inter } from "@next/font/google";
 import "@/styles/global.css";
 import clsx from "clsx";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,7 +15,8 @@ export default function DashboardRootLayout({ children }) {
     <html lang="en" className={inter.variable}>
       <head />
       <body className="h-screen w-screen candy-mesh p-6">
-        <GlassPane className="w-full h-full flex justify-center item-center">
+        <GlassPane className="w-full h-full flex item-center">
+          <Sidebar />
           {children}
         </GlassPane>
       </body>
